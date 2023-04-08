57de9e10839cee5b12ef3c5e2c3ee72c43ae6ab1
DANGQUOCSON <dangquocson1995@gmail.com>
2023-04-08 16:11:31 +0700

    init

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,9 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
